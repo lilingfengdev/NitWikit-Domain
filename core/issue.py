6276e85882ea988957f6f25d7de76d330fb5107c
lilingfengdev <lilingfengdev@outlook.com>
2024-10-04 21:58:26 +0800

    fix workflow

--- core/issue.py
+++ core/issue.py
@@ -6,16 +6,16 @@ class GithubIssue:
     def __init__(self):
         self.repo = github.Github(GitHubInfo.token).get_repo("lilingfengdev/NitWikit-Domain")
         self.issue = self.repo.get_issue(GitHubInfo.issue_number)
-        self.issue.lock("不接受评论")
+        self.issue.lock("too heated")
 
     def create_commit(self, body):
         self.issue.unlock()
         self.issue.create_comment(body)
-        self.issue.lock("不接受评论")
+        self.issue.lock("too heated")
 
     def close(self):
+        self.issue.lock("resolved")
         self.issue.edit(state='closed')
-        self.issue.lock("已完成")
 
     def owner(self):
         return self.issue.user
